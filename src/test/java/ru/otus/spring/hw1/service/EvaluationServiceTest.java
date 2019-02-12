fd264363d3810780c2b5e41865525c5d85460458
snirinn <snirinn@yandex-team.ru>
2019-02-12 19:24:45 +0300

    Spring boot1

--- src/test/java/ru/otus/spring/hw1/service/EvaluationServiceTest.java
+++ src/test/java/ru/otus/spring/hw1/service/EvaluationServiceTest.java
@@ -2,8 +2,9 @@ package ru.otus.spring.hw1.service;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import org.junit.Before;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import ru.otus.spring.hw1.entity.Exam;
 import ru.otus.spring.hw1.entity.Student;
 import ru.otus.spring.hw1.entity.Task;
@@ -12,18 +13,20 @@ import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
+@SpringBootTest
 public class EvaluationServiceTest {
     private static Map<String, String> QUESTIONS = ImmutableMap.of(
             "q1", "a1",
             "q2", "a2"
     );
 
+    @Autowired
     private EvaluationService evaluationService;
 
-    @Before
-    public void setUp() throws Exception {
-        evaluationService = new EvaluationServiceImpl();
-    }
+//    @Before
+//    public void setUp() throws Exception {
+//        evaluationService = new EvaluationServiceImpl();
+//    }
 
     @Test
     public void evaluate() {
