b1e64f4b04e92b89ea4e65d34824a13eecb24f6a
snirinn <snirinn@yandex-team.ru>
2019-02-14 14:40:20 +0300

    Spring boot - config test

--- src/test/java/ru/otus/spring/hw1/service/ServicesConfig.java
+++ src/test/java/ru/otus/spring/hw1/service/ServicesConfig.java
@@ -1,8 +1,9 @@
-package ru.otus.spring.hw1.config;
+package ru.otus.spring.hw1.service;
 
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
+import ru.otus.spring.hw1.config.YamlProps;
 
 @SpringBootConfiguration
 @ComponentScan({"ru.otus.spring.hw1.service", "ru.otus.spring.hw1.config"})
